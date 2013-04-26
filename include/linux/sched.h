fdf3eb945590644ff71eb21bf939b9c308b96544
Waiman Long <Waiman.Long@hp.com>
2013-04-25 21:06:20 -0700

    mutex: Move mutex spinning code from sched/core.c back to mutex.c
    Signed-off-by: Waiman Long<Waiman.Long@hp.com>
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: nicholas <firepoison35@gmail.com>
    
    Conflicts:
    	include/linux/sched.h
    	kernel/sched/features.h

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -361,7 +361,7 @@ extern signed long schedule_timeout_interruptible(signed long timeout);
 extern signed long schedule_timeout_killable(signed long timeout);
 extern signed long schedule_timeout_uninterruptible(signed long timeout);
 asmlinkage void schedule(void);
-extern int mutex_spin_on_owner(struct mutex *lock, struct thread_info *owner);
+extern void schedule_preempt_disabled(void);
 
 struct nsproxy;
 struct user_namespace;
